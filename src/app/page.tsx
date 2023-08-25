08d8ee77137ee7a53d2953e5ec927c2b72c8adbf
Sebastian Paulis <sebastian4paulis@gmail.com>
2023-08-25 18:53:51 -0400

    Initial implementation openlayers

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,33 @@
+'use client';
 import { Box, List, ListItem, ListItemText, Paper, Stack, TextField, Typography } from '@mui/material'
-import StarIcon from '@mui/icons-material/Star';
+import StarIcon from '@mui/icons-material/Star'
+import React, { useState, useEffect } from "react";
+import Map from "../Map";
+import { Layers, TileLayer } from "../Layers";
+import { osm } from "../Source";
+import { fromLonLat } from "ol/proj";
+import { Controls, FullScreenControl } from "../Controls";
+import mapConfig from "./config.json";
 
 var launches = ["Saturn 07", "Forger 11", "Spix 73"];
 
 export default function Home() {
+
+  const [center, setCenter] = useState(mapConfig.center);
+  const [zoom, setZoom] = useState(9);
+  const [domLoaded, setDomLoaded] = useState(false);
+
+  useEffect(() => {
+    setDomLoaded(true);
+  }, []);
+
+  if (!domLoaded){
+    return <div></div>
+  }
+
   return (
-    <Box sx={{ width: "100vw", height: "100vh" }}>
-      <Stack direction="row" justifyContent="space-between" sx={{ padding: 3 }}>
+    <Box sx={{ width: "100vw", height: "100vh", pointerEvents: "none"}}>
+      <Stack direction="row" justifyContent="space-between" sx={{ padding: 3, pointerEvents: "auto", position: "relative", zIndex: "2"}}>
 
         <Paper elevation={10}>
           <Stack direction="column">
@@ -27,12 +48,24 @@ export default function Home() {
           <StarIcon></StarIcon>
           <Typography>Launch Locations</Typography>
         </Stack>
-        
+
         <form>
           <TextField placeholder='Enter your Location'></TextField>
         </form>
 
       </Stack>
+
+      {domLoaded && 
+      <Box sx={{position: "absolute", top: 0, right: 0, width: 1, height: 1, zIndex: 1, pointerEvents: "auto"}}>
+        <Map center={fromLonLat(center)} zoom={zoom}>
+          <Layers>
+            <TileLayer source={osm()} zIndex={0} />
+          </Layers>
+          <Controls>
+            <FullScreenControl />
+          </Controls>
+        </Map>
+      </Box>}
     </Box>
   )
 }
