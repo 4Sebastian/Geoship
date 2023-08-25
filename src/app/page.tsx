c6f48356456ae52318cd2c1a39aa332f030d3fdc
SuperOwl52 <nicholas52.hartog@gmail.com>
2023-08-25 19:00:21 -0400

    initial implementation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,44 @@
+'use client';
 import { Box, List, ListItem, ListItemText, Paper, Stack, TextField, Typography } from '@mui/material'
 import StarIcon from '@mui/icons-material/Star';
+import axios from 'axios';
 
-var launches = ["Saturn 07", "Forger 11", "Spix 73"];
+import { useEffect, useState } from 'react';
+
 
 export default function Home() {
+
+  const [launches, setLaunches] = useState([{ "id": -1,
+                                            "vehicle": {
+                                             "id": -1,
+                                             "name": "Loading...",}
+                                            }]);
+
+  // Similar to componentDidMount and componentDidUpdate:
+  useEffect(() => {
+    // Update the document title using the browser API
+    document.title = `GeoShip`;
+    axios.get('https://fdo.rocketlaunch.live/json/launches/next/5')
+      .then(function (response) {
+        // handle success
+        var rockets = []
+        for (let index = 0; index < response.data.count; index++) {
+          //console.log(response.data.result[index].vehicle.name);
+          rockets[index] = response.data.result[index]
+        }
+        console.log(rockets)
+        setLaunches(rockets);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+      .finally(function () {
+        // always executed
+      });
+
+  }, []);
+
   return (
     <Box sx={{ width: "100vw", height: "100vh" }}>
       <Stack direction="row" justifyContent="space-between" sx={{ padding: 3 }}>
@@ -15,8 +50,8 @@ export default function Home() {
             </Paper>
             <List>
               {launches.map((value) => (
-                <ListItem key={value}>
-                  <ListItemText primary={`Rocket: ${value}`} />
+                <ListItem key={value.id}>
+                  <ListItemText primary={`Rocket: ${value.vehicle.name}`} />
                 </ListItem>
               ))}
             </List>
