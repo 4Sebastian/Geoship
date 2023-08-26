aeaaaf2720f14a4a1df86e15464504522cf5d00b
Sebastian Paulis <sebastian4paulis@gmail.com>
2023-08-26 14:56:14 -0400

    Initial implementation of autocomplete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { Box, List, ListItem, ListItemText, Paper, Stack, TextField, Typography } from '@mui/material'
+import { Box, List, ListItem, ListItemButton, ListItemText, Paper, Stack, TextField, Typography } from '@mui/material'
 
 import StarIcon from '@mui/icons-material/Star'
 import React, { useState, useEffect } from "react";
@@ -10,7 +10,7 @@ import { fromLonLat } from "ol/proj";
 import { Controls, FullScreenControl } from "../Controls";
 import mapConfig from "./config.json";
 import axios from 'axios';
-
+import { GeoapifyContext, GeoapifyGeocoderAutocomplete } from '@geoapify/react-geocoder-autocomplete';
 
 
 export default function Home() {
@@ -24,14 +24,67 @@ export default function Home() {
     }
   ]);
   const [center, setCenter] = useState(mapConfig.center);
-  const [zoom, setZoom] = useState(9);
+  const [zoom, setZoom] = useState(5);
+  const [type, setType] = useState();
+  const [language, setLanguage] = useState();
+  const [position, setPosition] = useState();
+  const [countryCodes, setCountryCodes] = useState();
+  const [limit, setLimit] = useState(5);
+  const [displayValue, setDisplayValue] = useState("");
   const [domLoaded, setDomLoaded] = useState(false);
+  const [address, setAddress] = useState("");
+  type addressSuggesstion = {
+    "properties": {
+      "formatted": ""
+    }
+  }
+
+  const [addressSuggestions, setAddressSuggesstions] = useState<addressSuggesstion[]>([]);
 
   useEffect(() => {
     setDomLoaded(true);
   }, []);
 
-  
+  useEffect(() => {
+    if (address != "") {
+      axios(getAddressConfig(address))
+        .then(function (response) {
+          console.log(response.data);
+          setAddressSuggesstions(response.data.features)
+        }).catch((err) => {
+          console.log("oops")
+          console.log(err)
+        })
+    }
+
+  }, [address])
+
+  function getAddressConfig(text: string) {
+    return {
+      method: 'get',
+      url: `https://api.geoapify.com/v1/geocode/autocomplete?text=${text}&apiKey=6996746ace5b4605a490e1625c60f468`,
+      headers: {}
+    };
+  }
+
+  function doesNotContainAddress(text: string) {
+    for (const element of addressSuggestions) {
+      if (element.properties.formatted == text) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  function onPlaceSelect(value: any) {
+    console.log(value);
+  }
+
+  function onSuggectionChange(value: any) {
+    console.log(value);
+  }
+
+
   // Similar to componentDidMount and componentDidUpdate:
   useEffect(() => {
     // Update the document title using the browser API
@@ -63,33 +116,52 @@ export default function Home() {
 
   return (
     <Box sx={{ width: "100vw", height: "100vh", pointerEvents: "none" }}>
-      <Stack direction="row" justifyContent="space-between" sx={{ padding: 3, pointerEvents: "auto", position: "relative", zIndex: "2" }}>
+      <Stack direction="column" justifyContent="space-between" sx={{ width: 1, height: 1, position: "relative", zIndex: 2 }}>
+        <Stack direction="row" justifyContent="space-between" sx={{ padding: 3 }}>
 
-        <Paper elevation={10}>
-          <Stack direction="column">
-            <Paper elevation={2} sx={{ padding: 1 }}>
-              <Typography>Upcoming Launches</Typography>
-            </Paper>
-            <List>
-              {launches.map((value) => (
-                <ListItem key={value.id}>
-                  <ListItemText primary={`Rocket: ${value.vehicle.name}`} />
-                </ListItem>
-              ))}
-            </List>
-          </Stack>
-        </Paper>
+          <Paper elevation={10} sx={{ height: "min-content", pointerEvents: "auto" }}>
+            <Stack direction="column">
+              <Paper elevation={2} sx={{ padding: 1 }}>
+                <Typography>Upcoming Launches</Typography>
+              </Paper>
+              <List>
+                {launches.map((value) => (
+                  <ListItem key={value.id}>
+                    <ListItemText primary={`Rocket: ${value.vehicle.name}`} />
+                  </ListItem>
+                ))}
+              </List>
+            </Stack>
+          </Paper>
 
-        <Stack direction="row" spacing={1}>
-          <StarIcon></StarIcon>
-          <Typography>Launch Locations</Typography>
+              <Paper elevation={10} sx={{ width: 0.4, height: "min-content", pointerEvents: "auto" }}>
+                <Paper elevation={2} sx={{ width: 1}}>
+                  <TextField fullWidth sx={{display: "inline-block"}} placeholder='Enter your Location' value={address} onChange={(event) => setAddress(event.target.value)}></TextField>
+                </Paper>
+                {doesNotContainAddress(address) && addressSuggestions.length > 0 && <List>
+                  {addressSuggestions.map((value) => (
+                    <ListItem key={value.properties.formatted}>
+
+                      <ListItemButton onClick={() => setAddress(value.properties.formatted)}>
+                        <ListItemText primary={`${value.properties.formatted}`} />
+                      </ListItemButton>
+                    </ListItem>
+                  ))}
+                </List>}
+              </Paper>
+
         </Stack>
 
-        <form>
-          <TextField placeholder='Enter your Location'></TextField>
-        </form>
-
+        <Stack direction="column" alignItems="flex-end" sx={{ margin: 3 }}>
+          <Paper elevation={10} sx={{ width: "max-content", padding: 1 }}>
+            <Stack direction="row" spacing={1}>
+              <StarIcon></StarIcon>
+              <Typography>Launch Locations</Typography>
+            </Stack>
+          </Paper>
+        </Stack>
       </Stack>
+
 
       {domLoaded &&
         <Box sx={{ position: "absolute", top: 0, right: 0, width: 1, height: 1, zIndex: 1, pointerEvents: "auto" }}>
