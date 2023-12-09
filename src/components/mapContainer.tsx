07825d9daf18f949324de2cfcdce3517edbed334
Sebastian Paulis <sebastian4paulis@gmail.com>
2023-12-08 21:23:07 -0500

    Gradient Working

--- src/components/mapContainer.tsx
+++ src/components/mapContainer.tsx
@@ -20,12 +20,12 @@ import { StyleFunction, StyleLike } from 'ol/style/Style';
 import { Coordinate } from 'ol/coordinate';
 
 
-export default function MapContainer(props: { address: any , launches: any, selectedRocketIndex: any}) {
+export default function MapContainer(props: { address: any, launches: any, selectedRocketIndex: any }) {
     const raster = new TileLayer({
         source: new OSM(),
     });
     const [source] = useState(new VectorSource());
-    
+
     const [view] = useState(new View({
         center: [-11000000, 6600000],
         zoom: 20,
@@ -33,76 +33,11 @@ export default function MapContainer(props: { address: any , launches: any, sele
     const [addressFeature, setAddressFeature] = useState<Feature>(new Feature());
     const [launchFeature, setLaunchFeature] = useState<Feature>(new Feature());
 
-    const circleStyleFunction = (circleColor: string) => {
-
-        return new Style({
-            fill: new Fill({
-                color: circleColor,
-            }),
-            stroke: new Stroke({
-                color: '#ffcc33',
-                width: 3,
-            }),
-            image: new CircleStyle({
-                radius: 15,
-                fill: new Fill({
-                    color: '#ffcc33',
-                }),
-            }),
-        });
-        // Default style if radius doesn't match any condition
-        return new Style({
-            // Default style properties
-        });
-    };
-
-
-    const circleGradientStyleFunction = (opacity: number) => {
-        const canvas = document.createElement('canvas');
-        canvas.width = 32;
-        canvas.height = 32;
-        const context = canvas.getContext('2d');
-    
-        if (context) {
-            const gradient = context.createRadialGradient(16, 16, 0, 16, 16, 16);
-            gradient.addColorStop(0, 'yellow');
-            gradient.addColorStop(1, 'red');
-    
-            context.arc(16, 16, 15, 0, 2 * Math.PI);
-            context.fillStyle = gradient;
-            context.globalAlpha = opacity;
-            context.fill();
-    
-            return new Style({
-                image: new CircleStyle({
-                    radius: 15,
-                    fill: new Fill({
-                        color: context.createPattern(canvas, 'no-repeat'),
-                    }),
-                    stroke: new Stroke({
-                        color: '#ffcc33',
-                        width: 3,
-                    }),
-                }),
-            });
-        } else {
-            return new Style(); // Return default empty style if canvas context is not available
-        }
-    };
     const vector = new VectorLayer({
         source: source,
-        
-        // {
-        //     'fill-color': 'rgba(255, 0, 0, 0.2)',
-        //     'stroke-color': '#ffcc33',
-        //     'stroke-width': 3,
-        //     'circle-radius': 15,
-        //     'circle-fill-color': '#ffcc33',
-        // }
-
     });
 
-    
+
     useEffect(() => {
         new Map({
             controls: [],
@@ -113,7 +48,7 @@ export default function MapContainer(props: { address: any , launches: any, sele
     }, [])
 
     useEffect(() => {
-        if(props.address){
+        if (props.address) {
             source.removeFeature(addressFeature)
             var feat = new Feature({
                 geometry: new Point(fromLonLat(props.address.geometry.coordinates)),
@@ -122,46 +57,68 @@ export default function MapContainer(props: { address: any , launches: any, sele
             view.setZoom(20)
             setAddressFeature(feat)
             source.addFeature(feat);
-        } 
+        }
     }, [props.address])
 
     useEffect(() => {
-        if(props.launches){
+        if (props.launches) {
             console.log(props.launches);
-            for (let index = 0; index < props.launches.length; index++){
-                if(props.launches[index] && props.launches[index].length >= 2){
+            for (let index = 0; index < props.launches.length; index++) {
+                if (props.launches[index] && props.launches[index].length >= 2) {
                     console.log(index);
                     console.log(fromLonLat([props.launches[index][1], props.launches[index][0]]))
-                    //source.removeFeature(launchFeature)
-                    var feat = new Feature({
-                        geometry: new Circle(fromLonLat([props.launches[index][1], props.launches[index][0]]), 50000),
-                    })
-                    var feat2 = new Feature({
-                        geometry: new Circle(fromLonLat([props.launches[index][1], props.launches[index][0]]), 100000),
-                    })
-                    var feat3 = new Feature({
-                        geometry: new Circle(fromLonLat([props.launches[index][1], props.launches[index][0]]), 150000),
-                    })
-                    //view.setCenter(fromLonLat([props.launches[index][1], props.launches[index][0]]))
-                    //setLaunchFeature(feat)
-                    feat.setStyle(circleStyleFunction('rgba(255, 0, 0, 0.15)'));
-                    feat2.setStyle(circleStyleFunction('rgba(255, 165, 0, 0.15)'));
-                    feat3.setStyle(circleStyleFunction('rgba(255, 255, 0, 0.15)'));
-                    source.addFeature(feat);
-                    source.addFeature(feat2);
-                    source.addFeature(feat3);
-                }  
-            }  
-        } 
+
+                    const circleFeature = new Feature({
+                        geometry: new Circle(fromLonLat([props.launches[index][1], props.launches[index][0]]), 50000)
+                    });
+                    circleFeature.setStyle(
+                        new Style({
+                            renderer(coordinates, state) {
+                                const [[x, y], [x1, y1]] = coordinates as Coordinate[];
+                                const ctx = state.context;
+                                const dx = x1 - x;
+                                const dy = y1 - y;
+                                const radius = Math.sqrt(dx * dx + dy * dy);
+                                console.log(radius)
+
+                                const innerRadius = 0;
+                                const outerRadius = radius * 1.4;
+
+                                const gradient = ctx.createRadialGradient(
+                                    x,
+                                    y,
+                                    innerRadius,
+                                    x,
+                                    y,
+                                    outerRadius
+                                );
+                                gradient.addColorStop(0, 'rgba(0,255,0,0.2)');
+                                gradient.addColorStop(0.4, 'rgba(255,165,0,0.2)');
+                                gradient.addColorStop(0.7, 'rgba(255,0,0,0.2)');
+                                gradient.addColorStop(1, 'rgba(255,0,0,0)');
+                                ctx.beginPath();
+
+                                ctx.arc(x, y, radius, 0, 2 * Math.PI, true);
+                                ctx.fillStyle = gradient;
+                                ctx.fill();
+                            },
+                        })
+                    );
+
+
+                    source.addFeature(circleFeature);
+                }
+            }
+        }
     }, [props.launches])
 
     useEffect(() => {
         console.log(props.selectedRocketIndex)
-        if(props.selectedRocketIndex != null){
+        if (props.selectedRocketIndex != null) {
             console.log(props.launches[props.selectedRocketIndex])
             view.setCenter(fromLonLat([props.launches[props.selectedRocketIndex][1], props.launches[props.selectedRocketIndex][0]]))
             view.setZoom(10)
-        } 
+        }
     }, [props.selectedRocketIndex])
 
     return (
