dd12a749109b4739f6e8ab4286a7e8ba7e4363e9
Sebastian Paulis <sebastian4paulis@gmail.com>
2024-06-23 00:22:52 -0400

    refactor: Add geoapify env variable
    Previously, the api key for geoapify was hard coded into
    the codebase, whcih was poor for security concerns. This
    was changed to an environment variable and tested through
    an npm run dev.
    
    GEO-21

--- src/util/addressUtils.ts
+++ src/util/addressUtils.ts
@@ -14,7 +14,7 @@ export async function getValidAddress(address: string | string[] | undefined): P
 }
 
 export async function getAddressSuggestionsResponse(address: string): Promise<Response> {
-    return await fetch(`https://api.geoapify.com/v1/geocode/autocomplete?text=${address}&apiKey=6996746ace5b4605a490e1625c60f468`)
+    return await fetch(`https://api.geoapify.com/v1/geocode/autocomplete?text=${address}&apiKey=${process.env.GEOAPIFY_API_KEY}`)
 }
 
 export async function getAddressSuggestions(address: string): Promise<any> {
