26f94ed7a32f5f7d4cd0ca7a73c78e501f561cc1
Sebastian Paulis <sebastian4paulis@gmail.com>
2024-07-14 18:44:08 -0400

    feat: Add navbar
    The layout was pretty basic and didn't have a consistent location
    for static navigation to stay. Therefore, a basic navbar was
    added that offers a place for a profile, title, and logo to be
    housed. This was tested by checking if it renders and if it stays
    present in both the main page and map page.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,11 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import {Box, ThemeProvider} from "@mui/material";
+import {Box, Paper, Stack, ThemeProvider, Typography} from "@mui/material";
 import {AppRouterCacheProvider} from "@mui/material-nextjs/v13-appRouter";
 import theme from "@/app/theme";
+import Image from "next/image";
+import PersonIcon from '@mui/icons-material/Person';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,15 +21,34 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <AppRouterCacheProvider>
-          <ThemeProvider theme={theme}>
-            <Box sx={{ width: "100vw", height: "100vh"}}>
+    <body className={inter.className}>
+    <link rel="preload" href="/fonts/truenobd.otf" as="font" type="font/otf" crossOrigin="anonymous"/>
+    <link rel="preload" href="/fonts/truenolt.otf" as="font" type="font/otf" crossOrigin="anonymous"/>
+    <AppRouterCacheProvider>
+      <ThemeProvider theme={theme}>
+        <Box sx={{width: "100vw", height: "100vh"}}>
+          <Stack direction="column" sx={{width: 1, height: 1}}>
+            <Paper sx={{width: 1, height: "75px", paddingX: 1, zIndex: 2, borderRadius: 0}} elevation={10}>
+              <Stack direction="row"
+                     sx={{width: 1, height: 1}}
+                     alignItems="center"
+                     justifyContent="space-between">
+                <Stack sx={{width: 'fit-content', height: '50px'}} direction="row" alignItems="center" spacing={1}>
+                  <Image src={'/GeoshipNoTitle.jpg'} alt={'GEOShip Logo'} width={50} height={50}
+                         style={{borderRadius: '5px'}}/>
+                  <Typography fontFamily="Trueno" variant="h4">GEOShip</Typography>
+                </Stack>
+                <PersonIcon sx={{fontSize: 50}}/>
+              </Stack>
+            </Paper>
+            <Box sx={{width: 1, height: 1}}>
               {children}
             </Box>
-          </ThemeProvider>
-        </AppRouterCacheProvider>
-      </body>
+          </Stack>
+        </Box>
+      </ThemeProvider>
+    </AppRouterCacheProvider>
+    </body>
     </html>
   )
 }
