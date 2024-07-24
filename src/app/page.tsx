07afdff1cc4dee57aa66709c0be6b4792cd10376
Sebastian Paulis <sebastian4paulis@gmail.com>
2024-07-23 21:39:04 -0400

    feat: Add contact section to home page
    The home page is under development, and the contact section has
    now been outlined. It does not function, but it has all the form
    inputs for future use. This was not tested besides being displayed
    on the home page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 "use server";
-import {Box, Button, Stack, Typography} from '@mui/material'
+import {Box, Button, Paper, Stack, TextField, Typography} from '@mui/material'
 
 import Image from "next/image";
+import Grid2 from "@mui/material/Unstable_Grid2";
 
 
 export default async function Home() {
@@ -34,9 +35,40 @@ export default async function Home() {
             {/*<Box sx={{ width: 1, height: '100%', backgroundColor: '#00771c'}} >*/}
             {/*    <Typography>Rocket Image Pagination</Typography>*/}
             {/*</Box>*/}
-            {/*<Box sx={{ width: 1, height: '100%', backgroundColor: '#08e2ff'}} >*/}
-            {/*    <Typography>Contact Me Section</Typography>*/}
-            {/*</Box>*/}
+            <Box sx={{ width: 1, height: '100%'}} >
+                <Box sx={{ width: 1, height: 1}} display={'flex'} justifyContent={'center'} alignItems={'center'}>
+                    <Stack direction={'column'} sx={{width: 1}} justifyContent={'center'} alignItems={'center'} spacing={20}>
+                        <Typography fontFamily={'Trueno'} variant={'h2'} color={'white'}>Contact Us</Typography>
+                        <Stack direction={'row'} sx={{width: 1, height: 1}} alignItems={'center'}>
+                            <Box sx={{ width: 0.5, height: 1, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                                <Typography sx={{padding: 2}} align={'center'} variant={'h5'} color={'white'}>
+                                    At GEOShip, feedback and customer support is our top priority.
+                                    If there is anything thats missing or greatly enjoyed,
+                                    feel free to contact the GEOShip team with feedback, wanted features, bugs, general thanks, anything!
+                                </Typography>
+                            </Box>
+                            <Box sx={{ width: 0.5, height: 1, display: 'flex', justifyContent: 'center', alignItems: 'center', padding: 2 }}>
+                                <Stack direction={'column'} spacing={2} alignItems={'center'}>
+                                    <Paper sx={{padding: 2}}>
+                                        <Grid2 container spacing={2}>
+                                            <Grid2 xs={6}>
+                                                <TextField label={'Name'} fullWidth></TextField>
+                                            </Grid2>
+                                            <Grid2 xs={6}>
+                                                <TextField label={'Email'} fullWidth></TextField>
+                                            </Grid2>
+                                            <Grid2 xs={12}>
+                                                <TextField label={"Description"} multiline fullWidth minRows={4}></TextField>
+                                            </Grid2>
+                                        </Grid2>
+                                    </Paper>
+                                    <Button color={'primary'} variant={'contained'}>Send</Button>
+                                </Stack>
+                            </Box>
+                        </Stack>
+                    </Stack>
+                </Box>
+            </Box>
             {/*<Box sx={{ width: 1, height: '100%', backgroundColor: '#13c110'}} >*/}
             {/*    <Typography>Copy Right & Links</Typography>*/}
             {/*</Box>*/}
